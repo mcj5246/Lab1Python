29bb0e5a842c1111f466b015331ca94f8704acde
mcj5246 <mcj5246@psu.edu>
2020-09-04 01:10:36 +0000

    added unit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-temperature = input("Enter temperature in celsius: ")
+temperature = input("Enter temperature: ")
+unit = input("Enter unit in F/f or C/c: ")
 temperature = float(temperature)
 print(f"{temperature}\N{degree sign} in Celsius is equivalent to {temperature*1.8+32}\N{degree sign} Fahrenheit.")
