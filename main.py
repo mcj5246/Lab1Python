417b7ec25bd8e9e45c2ba160af0723fea3fa2022
mcj5246 <mcj5246@psu.edu>
2020-09-04 01:06:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+temperature = input("Enter temperature in celsius: ")
+temperature = float(temperature)
+print(f"{temperature}\N{degree sign} in Celsius is equivalent to {temperature*1.8+32}\N{degree sign} Fahrenheit.")
